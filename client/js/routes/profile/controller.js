4336d6018ab4521ca59171688084d9a41a285ff5
Ignasi <ignblanch@gmail.com>
2017-08-25 22:06:02 +0200

    add navigation to profile

--- client/js/routes/profile/controller.js
+++ client/js/routes/profile/controller.js
@@ -1,4 +1,7 @@
 /* global angular */
 
 angular.module('skylab_final')
-  .controller('ProfileController', function ($scope, $http) {})
+  .controller('ProfileController', function ($scope, $http) {
+    $scope.tv = true
+    $scope.movies=true
+  })
